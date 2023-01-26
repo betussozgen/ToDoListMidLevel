fddc41739b3e8f4f2cd4b802fffcc1c3087f0308
betussozgen <fatmabetulozgen@hotmail.com>
2023-01-26 22:09:56 +0300

    modifed

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const alertSuccess = document.querySelector('.alert-success')
 //events
 todoButton.addEventListener('click',addTodo)
 
+todoList.addEventListener('click', deleteCheck)
 
 
 
@@ -18,7 +19,25 @@ todoButton.addEventListener('click',addTodo)
 function addTodo(e){
     e.preventDefault() //tıklandığında sayfayı yenilemesini engelledim.
 
-    //create todo div.
+    const isEmpty = str => !str.trim().length; //boşluk kontrolü
+
+    if(isEmpty(todoInput.value)){
+        alertWarning.style.display = "block"
+        setTimeout(() => {
+            alertWarning.style.display = "none"
+        }, 1500);
+
+        //clear todo input value
+        todoInput.value = "";
+
+    }else{
+
+        alertSuccess.style.display = "block"
+        setTimeout(() => {
+            alertSuccess.style.display = "none"
+        }, 1500);
+
+        //create todo div.
     const todoDiv = document.createElement('div');
     todoDiv.classList.add("todo") //todoList içerisine classList ekledim.
 
@@ -26,16 +45,39 @@ function addTodo(e){
     const completedButton = document.createElement("button")
     completedButton.innerHTML = "<i class='fas fa-check-circle'></i>" //buton içerisine ikonu ekledim.
     completedButton.classList.add("completed-btn")
+    todoDiv.appendChild(completedButton)
 
     //create todo li
+    const newTodo = document.createElement("li")
+    newTodo.innerHTML = todoInput.value; //todoInput'un value'sını newTodo'nun içerisine at.
+    newTodo.classList.add("todo-item")
+    todoDiv.appendChild(newTodo)
+
+
 
     
      //check trash button - sağıdaki butonu oluşturdum. 
      const trashButton = document.createElement("button")
      trashButton.innerHTML = "<i class='fa fa-minus-circle'></i>" //buton içerisine ikonu ekledim.
      trashButton.classList.add("trash-btn")
+     todoDiv.appendChild(trashButton)
 
-    console.log(completedButton)
+
+     //append to list
+     todoList.appendChild(todoDiv)
+
+     
+
+    console.log(todoDiv)
     console.log(trashButton)
+        
+    }
+
+    
+    
+}
+
+
+function deleteCheck(e){
     
 }
